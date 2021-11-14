87b694c738faaaccd75ff9bc321a9ff071ba3f0e
reethikamudunuri <69729142+reethikamudunuri@users.noreply.github.com>
2021-11-14 17:38:17 +0100

    Update kuberntes-task.py

--- kuberntes-task.py
+++ kuberntes-task.py
@@ -2,10 +2,7 @@ from kubernetes import client, config
 
 class Pod(object):
     
-    def __init__(self,name,images,label):
-        self.name=name
-        self.images=images
-        self.label=label
+    def __init__(self):
         config.load_kube_config()
         v1 = client.CoreV1Api()
         self.pods = v1.list_pod_for_all_namespaces(watch=False)
