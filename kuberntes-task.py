bd8023c476de1e2eded879830eb9c5aab3d569aa
reethikamudunuri <69729142+reethikamudunuri@users.noreply.github.com>
2021-11-14 15:52:38 +0100

    Update kuberntes-task.py

--- kuberntes-task.py
+++ kuberntes-task.py
@@ -1,7 +1,17 @@
 from kubernetes import client, config
 
 class Pod(object):
-    def __init__(self):
+    
+    def __init__(self,name,images,label):
+        self.name=name
+        self.images=imaages
+        self.label=label
         config.load_kube_config()
         v1 = client.CoreV1Api()
         self.pods = v1.list_pod_for_all_namespaces(watch=False)
+        
+     def get_images(self,pod):
+        images=[]
+        for i in pod.spec.containers:
+            images.append(i.image)
+        return images
